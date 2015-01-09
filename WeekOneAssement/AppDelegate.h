766872169da739b013fbd364f1a590a0b2464300
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-09 09:15:41 -0800

    Initial Commit

---
+++ WeekOneAssement/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeekOneAssement
+//
+//  Created by Evan Vandenberg on 1/9/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeekOneAssement/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeekOneAssement
+//
+//  Created by Evan Vandenberg on 1/9/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
